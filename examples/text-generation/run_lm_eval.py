e866ba54240e326b790aca4f484c1b3c05f5b71b
Sun, Xuehao <xuehao.sun@intel.com>
2025-01-20 15:08:18 +0800

    use inc to eval
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- examples/text-generation/run_lm_eval.py
+++ examples/text-generation/run_lm_eval.py
@@ -103,21 +103,23 @@ def main() -> None:
     from model_adapter import HabanaModelAdapter
 
     with torch.no_grad():
-        lm = HabanaModelAdapter(tokenizer, model, args, generation_config)
-
-    from optimum.habana.utils import HabanaGenerationTime, get_hpu_memory_stats
-
-    with HabanaGenerationTime() as timer:
-        with torch.no_grad():
-            log_samples = args.log_samples
-            results = evaluator.simple_evaluate(lm, tasks=args.tasks, limit=args.limit_iters, log_samples=log_samples)
-        if args.device == "hpu":
-            import habana_frameworks.torch.hpu as torch_hpu
-
-            torch_hpu.synchronize()
+        from neural_compressor.evaluation.lm_eval import evaluate, LMEvalParser
+        eval_args = LMEvalParser(
+            model="hf", 
+            user_model=model,
+            tokenizer=tokenizer,
+            batch_size=1,
+            tasks=','.join(args.tasks),
+            device="hpu",
+            pad_to_buckets=True,
+            num_fewshot=5 if ("mmlu" in args.tasks) else 0,
+        )
+        results = evaluate(eval_args)
+    if args.device == "hpu":
+        import habana_frameworks.torch.hpu as torch_hpu
+        torch_hpu.synchronize()
 
     results["args"] = vars(args)
-    results["duration"] = timer.last_duration
 
     if args.local_rank == 0:
         if args.device == "hpu":
